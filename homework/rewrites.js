c0dd3cbb96c412a4dabf5881a73b9b8aa69afbdb
Dan Gelok <dgelok@gmail.com>
2020-07-15 18:14:09 -0500

    refactoring to callback functions

--- homework/rewrites.js
+++ homework/rewrites.js
@@ -1,29 +1,46 @@
-// function add(x, y) {
-//     var result = x + y;
-//     return result;
-// }
-
-// function greeting(person) {
-// return 'Hola, ' + person + '!';
-// }
-
-// function product(numbers) {
-// return numbers.reduce(function(a, b) {
-//     return a * b;
-// }, 1);
-// } 
-
+function add(x, y, callback) {
+    setTimeout(()=>{
+        callback(x+y)
+    }, 1000)
+}
 
 function subtract(x, y, callback) {
     setTimeout(()=>{
             callback(x-y)
-    }, 1)
-
+    }, 1000)
 }
 
-subtract(10, 5, function(result){
-    console.log(result)
-})
+function greeting(person, callback) {
+    setTimeout(()=>{
+        callback('Hola, ' + person + '!');
+    }, 1000)
+}
+
+function product(numbers) {
+return numbers.reduce(function(a, b) {
+    return a * b;
+}, 1);
+} 
+
+myArr = [5,10]
+
+console.log(product(myArr))
+
+
+
+
+
+// add(2, 2, result => {
+//     console.log(result)
+// })
+
+// subtract(10, 5, function(result){
+//     console.log(result)
+// })
+
+// greeting("Manuel", function(result) {
+//     console.log(result)
+// })
 
 
 
