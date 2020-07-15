b8fb9169fe9e3d4cd5420f2a2228f3819a137d7a
Dan Gelok <dgelok@gmail.com>
2020-07-15 13:47:04 -0500

    question 1 solved

--- homework/capFile.js
+++ homework/capFile.js
@@ -1,10 +1,21 @@
 const fs = require('fs')
 
-let text = 'file1.txt'
+const readline = require("readline");
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
 
-fs.readFile(text, (error, buffer)=>{
-    if (error) {
-        console.log(error)
-    }
-    console.log(buffer.toString().toUpperCase())
+rl.question("Which file do you want to read? ", function(name) {
+    fs.readFile(name, (error, buffer)=>{
+        if (error) {
+            console.log(`ERROR: no such file: ${name}`)
+            return;
+        }
+        console.log(buffer.toString().toUpperCase())
+    })
+    rl.close();
 })
+
+
+
