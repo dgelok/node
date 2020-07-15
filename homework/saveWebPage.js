c59b7bed03ccdd547a47085c3e9b41322b3e8c08
Dan Gelok <dgelok@gmail.com>
2020-07-15 14:18:01 -0500

    Question 4 setup

---
+++ homework/saveWebPage.js
@@ -0,0 +1,23 @@
+const fs = require('fs')
+const dns = require('dns');
+const readline = require("readline");
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.question("Domain name: ", (domain)=>{
+    rl.question("Output file: ", (outFile)=>{
+        // do stuff
+        
+        dns.lookup(domain, (error, answer)=>{
+            if (error) {
+                console.log(`Error in retrieving ${domain}`);
+                return;
+            }
+            console.log(`IP Address: ${answer}`)
+        })
+        rl.close();
+    })
+    
+})
